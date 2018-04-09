71469cfa9cf4991bcd00e4db064883a9b928b4e2
iMobie-ZL <964664795@qq.com>
2018-04-09 20:10:05 +0800

    updated

--- ZLDropDownView/ZLDropDownView/ZLDropDownView.m
+++ ZLDropDownView/ZLDropDownView/ZLDropDownView.m
@@ -14,16 +14,24 @@
 #define ZLScreenWidth [UIScreen mainScreen].bounds.size.width
 #define ZLScreenHeight [UIScreen mainScreen].bounds.size.height
 
+/**  屏幕适配，这里是以iphone6的宽度作为标准的，当然也可以使用其他机型的宽度或者是高度 ***/
+#define ZLFitRealValue(value) ((value)/750.0f*[UIScreen mainScreen].bounds.size.width)
+
 static UIWindow *_window = nil;
+
 
 @implementation ZLDropDownView
 
 + (void)setup {
-    _window = [[UIWindow alloc] initWithFrame:ZLScreenBounds];
+    if (!_window) {
+        _window = [[UIWindow alloc] initWithFrame:ZLScreenBounds];
+    }
     _window.windowLevel = UIWindowLevelNormal;
-    _window.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.25];
+    _window.backgroundColor = [UIColor clearColor];
     _window.hidden = NO;
 }
 
-
++ (void)showWithView:(UIView *)wView belowView:(UIView *)bView {
+    CGRect cFrame = [bView convertRect:bView.frame toView:_window];
+}
 @end
